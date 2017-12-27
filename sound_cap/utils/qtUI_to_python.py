3420b2d381101c49a2c5cca0cd4328af580339f2
Mateusz Woś <wos.mateusz16@gmail.com>
2017-12-27 20:43:00 +0100

    qt xml file converter to python file

---
+++ sound_cap/utils/qtUI_to_python.py
@@ -0,0 +1,17 @@
+from PyQt4 import uic
+from sys import argv
+
+
+def ui_file_python_convert(file_name: str):
+    try:
+        with open(file_name, 'r') as qtUi:
+            with open(file_name.replace('.ui', '.py'), 'w') as pythonUI:
+                uic.compileUi(qtUi, pythonUI, execute=False)
+    except Exception as e:
+        print(str(e))
+
+
+try:
+    ui_file_python_convert(argv[1])
+except IndexError:
+    print("File argument not specified")
